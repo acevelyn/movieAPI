3ea7bebe63c59e9e6a10807c88089306d06c4f20
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-10-10 11:04:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,22 +16,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require("cors");
-let allowedOrigins = ["http://localhost:8080", "http://localhost:1234"];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // if a specific origin isn't found on the list of allowed origins...
-        let message =
-          "The CORS polict for this application doesn't allow access from origin " +
-          origin;
-        return callback(newError(message), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
+app.use(cors());
+// let allowedOrigins = ["http://localhost:8080", "http://localhost:1234"];
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // if a specific origin isn't found on the list of allowed origins...
+//         let message =
+//           "The CORS polict for this application doesn't allow access from origin " +
+//           origin;
+//         return callback(newError(message), false);
+//       }
+//       return callback(null, true);
+//     }
+//   })
+// );
 
 require("./auth")(app);
 const passport = require("passport");
